eadc0b8edc7045b1b7e655861008708beefcec31
EmilieAlice <ewaille@hotmail.fr>
2015-03-19 15:15:29 +0100

    Petit git paske tu flip ahah

--- Planning/src/fenetre/Planning.java
+++ Planning/src/fenetre/Planning.java
@@ -22,6 +22,7 @@ import javax.swing.JLabel;
 
 
 
+
 import dao.HeuresSessionModuleDao;
 import dao.ModuleDao;
 import dao.SessionDao;
@@ -49,6 +50,12 @@ import javax.swing.border.MatteBorder;
 import java.awt.Component;
 
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class Planning {
 
@@ -61,34 +68,34 @@ public class Planning {
 	private JPanel planning = new JPanel();
 	private JPanel plageLundi = new JPanel();
 	private JPanel nomLundi = new JPanel();
-	private JLabel lblLundi = new JLabel("   Lundi");
+	private JLabel lblLundi = new JLabel("  Lundi");
 	private JLabel lblDateLundi = new JLabel("");
-	private JPanel plageLundiMatin = new JPanel();
-	private JPanel plageLundiApresMidi = new JPanel();
+	private JLabel plageLundiMatin = new JLabel();
+	private JLabel plageLundiApresMidi = new JLabel();
 	private JPanel plageMardi = new JPanel();
 	private JPanel nomMardi = new JPanel();
-	private JLabel lblMardi = new JLabel("   Mardi");
+	private JLabel lblMardi = new JLabel("  Mardi");
 	private JLabel lblDateMardi = new JLabel("");
-	private JPanel plageMardiMatin = new JPanel();
-	private JPanel plageMardiApresMidi = new JPanel();
+	private JLabel plageMardiMatin = new JLabel();
+	private JLabel plageMardiApresMidi = new JLabel();
 	private JPanel plageMercredi = new JPanel();
 	private JPanel nomMercredi = new JPanel();
-	private JLabel lblMercredi = new JLabel("   Mercredi");
+	private JLabel lblMercredi = new JLabel("  Mercredi");
 	private JLabel lblDateMercredi = new JLabel("");
-	private JPanel plageMercrediMatin = new JPanel();
-	private JPanel plageMercrediApresMidi = new JPanel();
+	private JLabel plageMercrediMatin = new JLabel();
+	private JLabel plageMercrediApresMidi = new JLabel();
 	private JPanel plageJeudi = new JPanel();
 	private JPanel nomJeudi = new JPanel();
 	private JLabel lblJeudi = new JLabel("   Jeudi");
 	private JLabel lblDateJeudi = new JLabel("");
-	private JPanel plageJeudiMatin = new JPanel();
-	private JPanel plageJeudiApresMidi = new JPanel();
+	private JLabel plageJeudiMatin = new JLabel();
+	private JLabel plageJeudiApresMidi = new JLabel();
 	private JPanel plageVendredi = new JPanel();
 	private JPanel nomVendredi = new JPanel();
 	private JLabel lblVendredi = new JLabel("   Vendredi");
 	private JLabel lblDateVendredi = new JLabel("");
-	private JPanel plageVendrediMatin = new JPanel();
-	private JPanel plageVendrediApresMidi = new JPanel();
+	private JLabel plageVendrediMatin = new JLabel();
+	private JLabel plageVendrediApresMidi = new JLabel();
 	private JPanel plageHoraire = new JPanel();
 	private JButton btnValider = new JButton("Valider");
 
@@ -122,34 +129,28 @@ public class Planning {
 	private void initialize() {
 		frame.getContentPane().setForeground(Color.WHITE);
 		frame.getContentPane().setBackground(new Color(222, 184, 135));
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(50, 50, 1000, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		calendrier.setBackground(new Color(222, 184, 135));
 		//frame.setResizable(true);
 
-		calendrier.setBounds(10, 10, 414, 220);
+		calendrier.setBounds(25, 25, 975, 575);
 		frame.getContentPane().add(calendrier);
 		calendrier.setLayout(null);
-
-		lblSemaine.setBounds(5, 2, 60, 20);
+		lblSemaine.setBounds(5, 5, 50, 20);
 		calendrier.add(lblSemaine);
-
-		numeroSemaine.setBounds(65, 2, 40, 20);
+		numeroSemaine.setBounds(55, 5, 15, 20);
 		calendrier.add(numeroSemaine);
-
-		lblMatiere.setBounds(128, 2, 45, 20);
+		lblMatiere.setBounds(500, 5, 50, 20);
 		calendrier.add(lblMatiere);
+		nomModule.setBounds(550, 5, 200, 20);
 		nomModule.setBackground(new Color(210, 180, 140));
-
-		nomModule.setBounds(174, 2, 200, 20);
 		nomModule.addItem(" Selectionner le module ");
 		calendrier.add(nomModule);
-		planning.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
+		planning.setBounds(0, 25, 975, 550);
 
 		planning.setBackground(SystemColor.activeCaption);
-		planning.setBounds(0, 30, 415, 190);
-		calendrier.add(planning);
 		planning.setLayout(null);
 
 		plageLundi.setBounds(15, 0, 80, 190);
@@ -194,14 +195,14 @@ public class Planning {
 		lblDateMardi.setBounds(63, 0, 20, 20);
 		nomMardi.add(lblDateMardi);
 		plageMardiMatin.setBackground(new Color(230, 230, 250));
-		plageMardiMatin.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
+		plageMardiMatin.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
 
-		plageMardiMatin.setBounds(0, 105, 80, 85);
+		plageMardiMatin.setBounds(0, 20, 80, 85);
 		plageMardi.add(plageMardiMatin);
 		plageMardiApresMidi.setBackground(new Color(230, 230, 250));
-		plageMardiApresMidi.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
+		plageMardiApresMidi.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
 
-		plageMardiApresMidi.setBounds(0, 20, 80, 85);
+		plageMardiApresMidi.setBounds(0, 105, 80, 85);
 		plageMardi.add(plageMardiApresMidi);
 
 		plageMercredi.setBounds(175, 0, 80, 190);
@@ -219,14 +220,14 @@ public class Planning {
 		lblDateMercredi.setBounds(63, 0, 20, 20);
 		nomMercredi.add(lblDateMercredi);
 		plageMercrediMatin.setBackground(new Color(230, 230, 250));
-		plageMercrediMatin.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
+		plageMercrediMatin.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
 
-		plageMercrediMatin.setBounds(0, 105, 80, 85);
+		plageMercrediMatin.setBounds(0, 20, 80, 85);
 		plageMercredi.add(plageMercrediMatin);
 		plageMercrediApresMidi.setBackground(new Color(230, 230, 250));
-		plageMercrediApresMidi.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
+		plageMercrediApresMidi.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
 
-		plageMercrediApresMidi.setBounds(0, 20, 80, 85);
+		plageMercrediApresMidi.setBounds(0, 105, 80, 85);
 		plageMercredi.add(plageMercrediApresMidi);
 
 		plageJeudi.setBounds(255, 0, 80, 190);
@@ -244,14 +245,14 @@ public class Planning {
 		lblDateJeudi.setBounds(63, 0, 20, 20);
 		nomJeudi.add(lblDateJeudi);
 		plageJeudiMatin.setBackground(new Color(230, 230, 250));
-		plageJeudiMatin.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
+		plageJeudiMatin.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
 
-		plageJeudiMatin.setBounds(0, 105, 80, 85);
+		plageJeudiMatin.setBounds(0, 20, 80, 85);
 		plageJeudi.add(plageJeudiMatin);
 		plageJeudiApresMidi.setBackground(new Color(230, 230, 250));
-		plageJeudiApresMidi.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
+		plageJeudiApresMidi.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
 
-		plageJeudiApresMidi.setBounds(0, 20, 80, 85);
+		plageJeudiApresMidi.setBounds(0, 105, 80, 85);
 		plageJeudi.add(plageJeudiApresMidi);
 
 		plageVendredi.setBounds(335, 0, 80, 190);
@@ -269,19 +270,20 @@ public class Planning {
 		lblDateVendredi.setBounds(63, 0, 20, 20);
 		nomVendredi.add(lblDateVendredi);
 		plageVendrediMatin.setBackground(new Color(230, 230, 250));
-		plageVendrediMatin.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
+		plageVendrediMatin.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
 
-		plageVendrediMatin.setBounds(0, 105, 80, 85);
+		plageVendrediMatin.setBounds(0, 20, 80, 85);
 		plageVendredi.add(plageVendrediMatin);
 		plageVendrediApresMidi.setBackground(new Color(230, 230, 250));
-		plageVendrediApresMidi.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(210, 180, 140)));
+		plageVendrediApresMidi.setBorder(new MatteBorder(1, 0, 0, 0, (Color) new Color(210, 180, 140)));
 
-		plageVendrediApresMidi.setBounds(0, 20, 80, 85);
+		plageVendrediApresMidi.setBounds(0, 105, 80, 85);
 		plageVendredi.add(plageVendrediApresMidi);
 		plageHoraire.setBackground(SystemColor.inactiveCaption);
 
 		plageHoraire.setBounds(0, 0, 15, 190);
 		planning.add(plageHoraire);
+		calendrier.add(planning);
 
 		btnValider.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -319,14 +321,14 @@ public class Planning {
 		SelectionnePlage selectionnePlage = new SelectionnePlage();
 		plageLundiMatin.addMouseListener(selectionnePlage);
 		plageLundiApresMidi.addMouseListener(selectionnePlage);
-		plageMardiMatin.addMouseListener(selectionnePlage);
-		plageMardiApresMidi.addMouseListener(selectionnePlage);
-		plageMercrediMatin.addMouseListener(selectionnePlage);
 		plageMercrediApresMidi.addMouseListener(selectionnePlage);
-		plageJeudiMatin.addMouseListener(selectionnePlage);
 		plageJeudiApresMidi.addMouseListener(selectionnePlage);
-		plageVendrediMatin.addMouseListener(selectionnePlage);
-		plageVendrediApresMidi.addMouseListener(selectionnePlage);		
+		plageVendrediMatin.addMouseListener(selectionnePlage);		
+		plageMardiApresMidi.addMouseListener(selectionnePlage);
+		plageMardiMatin.addMouseListener(selectionnePlage);
+		plageMercrediMatin.addMouseListener(selectionnePlage);
+		plageJeudiMatin.addMouseListener(selectionnePlage);
+		plageVendrediApresMidi.addMouseListener(selectionnePlage);
 
 	}
 
@@ -334,31 +336,39 @@ public class Planning {
 
 		public void mouseClicked(MouseEvent e) {
 			Object module = nomModule.getSelectedItem();
-			System.out.println(module);
+			String string = module.toString();
+			String[] infos = string.split(" ");
+			plageJeudiApresMidi.setText("Séance de " + infos[1]);
+			plageJeudiMatin.setText("Séance de " + infos[1]);
+			/*
+			if(infos[1] == "Selectionner")
+				System.out.println("Selectionner un module");
+			else 
+				System.out.println("Séance de " + infos[1]);
+			 */
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
+			/*if(plageLundiMatin.getToolTipText() != null) 
+			System.out.println("plein");
+			else
+				System.err.println("vide");*/
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
+			//vide pour le moment
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
+			//vide pour le moment
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
+			//vide pour le moment
 		}
 	}
 }
