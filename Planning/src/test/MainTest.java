436a37856de3cf8a71084cfd33a059147c31e0ce
jerdopler <lebaronjerome@free.fr>
2015-04-01 18:03:12 +0200

    Résolution du conflit de MainTest

--- Planning/src/test/MainTest.java
+++ Planning/src/test/MainTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
@@ -10,27 +11,28 @@ import modele.Seance;
 public class MainTest {
 
 	public static void main(String[] args) {
-		
+
 		int milliSecondesParJour = (1000 * 60 * 60 * 24);
 
-		GregorianCalendar premierJour = new GregorianCalendar(2015, 05, 07);
+		GregorianCalendar premierJour = new GregorianCalendar(2015, 05, 07, 9,
+				0);
 		GregorianCalendar dernierJour = new GregorianCalendar(2016, 05, 12);
 
 		long nbreJours = (dernierJour.getTimeInMillis() - premierJour
 				.getTimeInMillis()) / milliSecondesParJour;
-		
+
 		System.out.println(nbreJours);
-		
+
 		ArrayList<Seance> listeSeance = new ArrayList<Seance>();
 		SeanceDao seanceDao = new SeanceDao();
 		listeSeance = seanceDao.findSeanceByIdSession(1);
 		GregorianCalendar jour = new GregorianCalendar(2015, 0, 12);
 		Seance.Creneau creneau = Seance.Creneau.MATIN;
-		
+
 		Seance seance = new Seance(1, 1, 1, jour, creneau, 4, "coucou");
-		
+
 		System.out.println(seance);
+
 	}
-	
 
 }
