b43c300231536866679f6619cd02c73729f5809c
jerdopler <lebaronjerome@free.fr>
2015-04-06 15:38:58 +0200

    De nombreux ajouts de commentaires pour la compréhension

--- Planning/src/test/SimpleTableDemo.java
+++ Planning/src/test/SimpleTableDemo.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -32,57 +33,78 @@ public class SimpleTableDemo extends JPanel {
 		long nbreJours = (dernierJour.getTimeInMillis() - premierJour
 				.getTimeInMillis()) / milliSecondesParJour;
 
+		// Pour formater une date au format désiré
 		SimpleDateFormat format = new SimpleDateFormat("dd/MM/YY");
 
-		String[] columnNames = {"Semaine", "LUNDI", "MARDI", "MERCREDI", "JEUDI",
-				"VENDREDI" };
+		// On remplit le nom des colonnes de titre
+		String[] columnNames = { "Semaine", "LUNDI", "MARDI", "MERCREDI",
+				"JEUDI", "VENDREDI" };
+
+		// On récupère le nombre de semaines correspodant au nombre de jours
 		int nbreSemaines = (int) (nbreJours / 7) + 1;
 
-		Object[][] data = new Object[nbreSemaines][6];
+		// On créé un tableau d'objet à deux dimensions pour remplir notre
+		// JTable
+		Object[][] data = new Object[nbreSemaines][columnNames.length];
 
+		// On créé trois parcoureurs pour remplir le tableau
 		int i = 0;
-		int j = 0;
-		int x = 1;
+		int numeroLigne = 0;
+		int numeroColonne = 1;
+
 		while (i <= nbreJours) {
-			Date d = premierJour.getTime();
-			String affiche = format.format(d);
+			Date date = premierJour.getTime();
+			String affiche = format.format(date);
 			int jourSemaine = premierJour.get(Calendar.DAY_OF_WEEK);
+
+			// Si le parcoureur de jours est divisible par 7 alors on passe à la
+			// semaine suivante
 			if (i != 0 & i % 7 == 0) {
-				j++;
+				numeroLigne++;
 			}
-			if (x != 1 & x % 6 == 0) {
-				x = 1;
+			// Si le numéro de la colonne est divisible par 6 alors on est à la
+			// ligne suivante donc on remet ce numéro à 1 pour écrire à Lundi
+			if (numeroColonne != 1 & numeroColonne % 6 == 0) {
+				numeroColonne = 1;
 			}
-			data[j][0] = premierJour.get(Calendar.WEEK_OF_YEAR);
+			// La première case de chaque ligne est toujours le numéro de la
+			// semaine
+			data[numeroLigne][0] = premierJour.get(Calendar.WEEK_OF_YEAR);
+
+			// Si le numéro correpondant au jour de la semaine n'est pas 1
+			// (dimanche) ou 7 (samedi) on ajoute des données au tableau
 			if (jourSemaine != 1 || jourSemaine != 7) {
-				
+
+				// On étudie chaque jour de la semaine, on remplit le tableau et
+				// on avance la colonne de 1
 				switch (jourSemaine) {
 				case 2:
-					data[j][x] = affiche;
-					x++;
+					data[numeroLigne][numeroColonne] = affiche;
+					numeroColonne++;
 					break;
 				case 3:
-					data[j][x] = affiche;
-					x++;
+					data[numeroLigne][numeroColonne] = affiche;
+					numeroColonne++;
 					break;
 				case 4:
-					data[j][x] = affiche;
-					x++;
+					data[numeroLigne][numeroColonne] = affiche;
+					numeroColonne++;
 					break;
 				case 5:
-					data[j][x] = affiche;
-					x++;
+					data[numeroLigne][numeroColonne] = affiche;
+					numeroColonne++;
 					break;
 				case 6:
-					data[j][x] = affiche;
-					x++;
+					data[numeroLigne][numeroColonne] = affiche;
+					numeroColonne++;
 					break;
 				default:
 					break;
 				}
 
 			}
-
+			// On ajoute une journée en milliseconde pour passer au jour suivant
+			// et on ajoute 1 au parcoureur
 			premierJour.setTimeInMillis(premierJour.getTimeInMillis()
 					+ milliSecondesParJour);
 			i++;
@@ -93,6 +115,11 @@ public class SimpleTableDemo extends JPanel {
 		final JTable table = new JTable(data, columnNames);
 		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		table.setFillsViewportHeight(true);
+
+		// Pour ajouter le mode de sélection unique de cellule à unique et à
+		// seulement une cellule
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setCellSelectionEnabled(true);
 
 		if (DEBUG) {
 			table.addMouseListener(new MouseAdapter() {
