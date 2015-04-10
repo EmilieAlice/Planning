cff89b29a24adcfdc8de57053ef1994508670fcb
EmilieAlice <ewaille@hotmail.fr>
2015-04-10 09:19:24 +0200

    Ajout classe Salle et SalleDao

---
+++ Planning/src/dao/SalleDao.java
@@ -0,0 +1,45 @@
+package dao;
+
+import java.sql.ResultSet;
+
+import modele.Module;
+import modele.Salle;
+
+public class SalleDao {
+
+	private static java.sql.PreparedStatement pFindSalleById = null;
+	/**
+	 * Requete pour récupérer une salle grace à son id
+	 */
+	static {
+		try {
+			pFindSalleById = DataBase.getConnection().prepareStatement(
+					"SELECT nom_salle FROM lagarenne2015.salle where id_salle =?;");
+		} catch (Exception e) {
+			e.getMessage();
+			System.out.println("Requete findSalleById échouée.");
+		}
+	}
+	/**
+	 * Méthode qui récupère dans la base données un objet Salle grace à son id
+	 * 
+	 * @param idSalle
+	 * @return Module
+	 */
+	public Salle findModuleByNom(int idSalle) {
+		Salle salle = new Salle();
+		try {
+			pFindSalleById.setInt(1, idSalle);
+			ResultSet resultat = pFindSalleById.executeQuery();
+			if (resultat.next()) {
+				salle.setIdSalle(resultat.getInt("id_salle"));
+				salle.setNomSalle(resultat.getString("nom_salle"));
+			} else {
+				salle = null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return salle;
+	}
+}
---
+++ Planning/src/modele/Salle.java
@@ -0,0 +1,39 @@
+package modele;
+
+public class Salle {
+
+	private int idSalle;
+	private String nomSalle;
+	
+	public Salle(int idSalle, String nomSalle) {
+		super();
+		this.idSalle = idSalle;
+		this.nomSalle = nomSalle;
+	}
+
+	public Salle() {
+	}
+
+	public int getIdSalle() {
+		return idSalle;
+	}
+
+	public void setIdSalle(int idSalle) {
+		this.idSalle = idSalle;
+	}
+
+	public String getNomSalle() {
+		return nomSalle;
+	}
+
+	public void setNomSalle(String nomSalle) {
+		this.nomSalle = nomSalle;
+	}
+
+	@Override
+	public String toString() {
+		return "Salle [idSalle=" + idSalle + ", nomSalle=" + nomSalle + "]";
+	}
+	
+	
+}
