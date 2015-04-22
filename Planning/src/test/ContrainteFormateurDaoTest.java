20b5faa207cdf9f951126e5458c87d34434f7520
EmilieAlice <ewaille@hotmail.fr>
2015-04-22 11:01:03 +0200

    ContrainteFormateurDaoTest OK

--- Planning/src/test/ContrainteFormateurDaoTest.java
+++ Planning/src/test/ContrainteFormateurDaoTest.java
@@ -2,13 +2,27 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import dao.ContrainteFormateurDao;
 
 public class ContrainteFormateurDaoTest {
 
 	@Test
 	public void testDonneJourCreneauIndispo() {
-		fail("Not yet implemented");
+		ArrayList<Integer> listeEssai = new ArrayList<Integer>();
+		listeEssai.add(4);
+		listeEssai.add(10);
+		
+		ArrayList<Integer> listeBase = new ArrayList<Integer>();
+		ContrainteFormateurDao contrainteDao = new ContrainteFormateurDao();
+		listeBase = contrainteDao.donneJourCreneauIndispo(5);
+		
+		for (int i = 0; i < listeBase.size(); i++) {
+			assertEquals(listeEssai.get(i), listeBase.get(i));
+		}
 	}
 
 }
